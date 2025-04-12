26d9dd7295b603af6aa1b754699e191fa17d7931
Habosa162 <habosa2000@hotmail.com>
2025-04-12 09:57:05 +0200

    BrandRepository

---
+++ Gym Community/Infrastructure/Interfaces/ECommerce/IBrandRepository.cs
@@ -0,0 +1,13 @@
+﻿using Gym_Community.Domain.Data.Models.E_comm;
+
+namespace Gym_Community.Infrastructure.Interfaces.ECommerce
+{
+    public interface IBrandRepository
+    {
+        public Task<Brand?> AddAsync(Brand brand);
+        public Task<IEnumerable<Brand>> ListAsync();
+        public Task<Brand?> GetById(int id);
+        public Task<Brand?> UpdateAsync(Brand brand);
+        public Task<bool> RemoveAsync(Brand brand); 
+    }
+}
---
+++ Gym Community/Infrastructure/Repositories/ECommerce/BrandRepository.cs
@@ -0,0 +1,58 @@
+﻿using Gym_Community.Domain.Data.Models.E_comm;
+using Gym_Community.Infrastructure.Context;
+using Gym_Community.Infrastructure.Interfaces.ECommerce;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gym_Community.Infrastructure.Repositories.ECommerce
+{
+    public class BrandRepository : IBrandRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public BrandRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Brand?> AddAsync(Brand brand)
+        {
+            await _context.Brands.AddAsync(brand);
+            if (await _context.SaveChangesAsync() > 0)
+            {
+                return brand;
+            }
+            else return null;
+        }
+        public async Task<IEnumerable<Brand>> ListAsync()
+        {
+            return await _context.Brands.ToListAsync(); 
+        }
+        public async Task<Brand?> GetById(int id)
+        {
+            return await _context.Brands.FirstOrDefaultAsync(b => b.BrandID == id);
+        }
+        public async Task<Brand?> UpdateAsync(Brand brand)
+        {
+            _context.Brands.Update(brand); 
+            if(await _context.SaveChangesAsync() > 0)
+            {
+                return brand;
+            }
+            else
+            {
+                return null; 
+            }
+        }
+
+        public async Task<bool> RemoveAsync(Brand brand)
+        {
+            _context.Brands.Remove(brand);
+            if( await _context.SaveChangesAsync() > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false; 
+            }
+        }
+    }
+}
