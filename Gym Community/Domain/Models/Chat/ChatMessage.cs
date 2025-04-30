a968e0149d8012cdb84e122c125e19c30ce80ab5
Shawky <shawkygamalcu@gmail.com>
2025-04-30 05:49:01 +0300

    add chat basic module

---
+++ Gym Community/Domain/Models/Chat/ChatMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace Gym_Community.Domain.Models.Chat
+{
+    public class ChatMessage
+    {
+        public int Id { get; set; }
+        public string SenderId { get; set; }
+        public string GroupId { get; set; } // Changed from ReceiverId to GroupId
+        public string Content { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
